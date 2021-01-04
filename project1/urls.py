83142e2cace75c9fe12a31aa3f7fed4b12e72123
wuzixuan <wuzixuan@xinye.com>
2021-01-04 19:28:12 +0800

    model，view，admin 测试

--- project1/urls.py
+++ project1/urls.py
@@ -18,5 +18,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /project1/
     path('', views.index, name='index'),
+    # ex: /project1/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/<int:second_id>/', views.detail, name='detail'),
+    # ex: /project1/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /project1/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- project1/views.py
+++ project1/views.py
@@ -2,7 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Person
 
 
 def index(request):
+    latest_question_list = Person.objects.order_by('id')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def index1(request):
+    #从数据库中取5条数据，按照id排序
+    latest_question_list = Person.objects.order_by('id')[:5]
+    #遍历上面的5条数据，取每条数据的first_name，追加到output后面
+    output = ', '.join([q.first_name for q in latest_question_list])
+    return HttpResponse(output)
+
+def index2(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % (question_id))
+
+#def detail(request, question_id,second_id):
+#    return HttpResponse("You're looking at question %s.%s" % (question_id, second_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
